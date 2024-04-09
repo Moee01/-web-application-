8fdd424178f62e575bf31a4629c38f70032ddc74
Moee01 <102093354+Moee01@users.noreply.github.com>
2024-04-09 22:30:25 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 docker pull moekharouf/web-app
 
-docker stop your_container_name
-docker rm your_container_name
+docker stop practical_chebyshev
+docker rm practical_chebyshev
 
 
 docker run -d --name your_container_name -p 8080:80 your_docker_username/your_image_name
