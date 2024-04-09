e2cdbbad90d0d269527eb71e9f2ceb82d959d01b
Moee01 <102093354+Moee01@users.noreply.github.com>
2024-04-09 22:16:55 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Parse the HTML template
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with data
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
