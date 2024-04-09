cd6ca8336bb9e979540c7dbe692c6765f303cdba
Moee01 <102093354+Moee01@users.noreply.github.com>
2024-04-09 22:04:23 +0300

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+docker pull moekharouf/web-app
+
+docker stop your_container_name
+docker rm your_container_name
+
+
+docker run -d --name your_container_name -p 8080:80 your_docker_username/your_image_name
